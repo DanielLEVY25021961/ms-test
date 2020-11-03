7bddfb85274e3870b8f5cd773390e5f04fa966f3
Daniel LEVY <dany.levy@free.fr>
2020-11-03 18:02:49 +0100

    Après modification des annotations sur la classe Application

--- src/main/java/levy/daniel/application/MsTestApplication.java
+++ src/main/java/levy/daniel/application/MsTestApplication.java
@@ -5,12 +5,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
- * @author daniel.levy
+ * CLASSE MsTestApplication :<br/>
+ * <ul>
+ * <li>Point d'entrée de l'application SPRING BOOT.</li>
+ * <li>L'annotation <code>SpringBootApplication</code> déclare à SPRING 
+ * qu'il s'agit de la classe APPLICATIVE.</li>
+ * <li>L'annotation <code>EnableConfigurationProperties</code> déclare à SPRING 
+ * que l'on peut écrire un Bean qui récupère les valeurs 
+ * dans les applications.properties.</li>
+ * <li>L'annotation <code>EnableDiscoveryClient</code> déclare à SPRING 
+ * que ce Micro-Service s'enregistre auprès du serveur annuaire EUREKA.</li>
+ * <li>L'annotation <code>EnableSwagger2</code> déclare à SPRING 
+ * que l'on va générer la documentation du MICRO-SERVICE avec Swagger2.</li>
+ * </ul>
+ * <br/>
  *
+ * - Exemple d'utilisation :<br/>
+ *<br/>
+ * 
+ * - Mots-clé :<br/>
+ * <br/>
+ *
+ * - Dépendances :<br/>
+ * <br/>
+ *
+ *
+ * @author Daniel Lévy
+ * @version 1.0
+ * @since 20 oct. 2020
  */
 @EnableConfigurationProperties
 @EnableDiscoveryClient
+@EnableSwagger2
 @SpringBootApplication
 public class MsTestApplication { // NOPMD by daniel.levy on 16/10/2020 06:46
 
