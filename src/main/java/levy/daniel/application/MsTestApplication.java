7b431b7fb5d0f3681edffaedbb3082eb2ecc370b
Daniel LEVY <dany.levy@free.fr>
2020-11-03 17:47:05 +0100

    Après modification du POM

--- src/main/java/levy/daniel/application/MsTestApplication.java
+++ src/main/java/levy/daniel/application/MsTestApplication.java
@@ -2,11 +2,15 @@ package levy.daniel.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author daniel.levy
  *
  */
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsTestApplication { // NOPMD by daniel.levy on 16/10/2020 06:46
 
